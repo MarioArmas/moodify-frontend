11150de45fa94d936674996afeb4f51b19ceb0d3
Mario Armas <mario.r.armas@gmail.com>
2025-05-18 20:16:21 -0600

    Add intersection observer

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,21 +1,40 @@
-import React from 'react'
+import { useState, useRef, useEffect } from 'react'
 //import { FiMusic } from 'react-icons/fi'
 import styles from './Song.module.css'
 
 export default function Song({ name, artist, url }) {
+  const ref = useRef()
+  const [visible, setVisible] = useState(false)
+
+  useEffect(() => {
+    const observer = new window.IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setVisible(true)
+          observer.disconnect()
+        }
+      },
+      { threshold: 0.1 }
+    )
+    if (ref.current) observer.observe(ref.current)
+    return () => observer.disconnect()
+  }, [])
+
   return (
     <div className={styles.container}>
-      <iframe
-        title={`${name} - ${artist}`}
-        style={{
-          backgroundColor: '#000',
-          color: '#000'
-        }}
-        src={url}
-        width="100%" height="80" frameBorder="0"
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-        loading="lazy">
-      </iframe>
+      {visible && (
+        <iframe
+          title={`${name} - ${artist}`}
+          style={{
+            backgroundColor: '#000',
+            color: '#000'
+          }}
+          src={url}
+          width="100%" height="80" frameBorder="0"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy">
+        </iframe>
+      )}
     </div>
   )
 }
