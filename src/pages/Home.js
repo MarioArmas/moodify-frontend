79a140fc5ba7889bf34b2f2c4d5c6f013b6f5bb8
Mario Armas <mario.r.armas@gmail.com>
2025-05-18 19:57:41 -0600

    Fix styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,13 @@ import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <main>
-      <h1>Moodify</h1>
-      <p className="subtitle">Discover the perfect melody for every emotion and let the music speak for you</p>
-      <Link to='/signup' className='btn'>Get started</Link>
-      <button className='btn sentry' onClick={() => {throw new Error("This is your first error!");}}>Break the world</button>
-    </main>
+    <div className='main-container'>
+      <main>
+        <h1>Moodify</h1>
+        <p className="subtitle">Discover the perfect melody for every emotion and let the music speak for you</p>
+        <Link to='/signup' className='btn'>Get started</Link>
+        <button className='btn sentry' onClick={() => {throw new Error("This is your first error!");}}>Break the world</button>
+      </main>
+    </div>
   );
 }
